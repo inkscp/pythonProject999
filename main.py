aca900e4e0406525dfc358da28e13b6bee16b9ec
Inna <inna.pshenichnikova@gmail.com>
2023-07-10 12:27:01 +0300

    static - error

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
 
 # каркасный вариант приложения
-from flask import Flask
+from flask import Flask, url_for  # подключаем конструктор и урл
 
 app = Flask(__name__)
 
@@ -32,6 +32,31 @@ def countdown():
     lst = [str(x) for x in range(10, 0, -1)]
     lst.append('Start')
     return '<br>'.join(lst)  #br - перенос на новую строку
+
+@app.route('/slogan')
+def index():
+    return 'Адмирал<br><a href="/slogan">Слоган</a>'
+
+@app.route('/poster')
+def poster():
+    return f"""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Постер</title>
+</head>
+<body>
+<h1>Постер к фильму</h1>
+<img src="{url_for('static', filename='images/tigre.jpg')}"  # 
+alt="Здесь должна была быть картинка, но не нашлась">
+<p>И крепка, как смерть, любовь</p>
+</body>
+</html>"""
+
+@app.route('/slogan')
+def slogan():
+    return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a>'
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
