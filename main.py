08c96e9d8102e2fb044974f96a7037dc9911203c
Inna <inna.pshenichnikova@gmail.com>
2023-07-10 14:14:37 +0300

    static - ok

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def countdown():
     return '<br>'.join(lst)  #br - перенос на новую строку
 
 @app.route('/slogan')
-def index():
+def slogan():
     return 'Адмирал<br><a href="/slogan">Слоган</a>'
 
 @app.route('/poster')
@@ -44,19 +44,33 @@ def poster():
 <head>
     <meta charset="UTF-8">
     <title>Постер</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
 </head>
 <body>
 <h1>Постер к фильму</h1>
-<img src="{url_for('static', filename='images/tigre.jpg')}"  # 
+<img src="{url_for('static', filename='images/tigre.jpg')}"
 alt="Здесь должна была быть картинка, но не нашлась">
-<p>И крепка, как смерть, любовь</p>
+<p class='red'>И крепка, как смерть, любовь</p>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </body>
 </html>"""
 
-@app.route('/slogan')
-def slogan():
-    return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a>'
+# @app.route('/slogan')
+# def slogan():
+#     return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a>'
 
+@app.route('/nekrasov')
+def nekrasov():
+    return
+<img src="{url_for('static', filename='images/nekrasov.jpg')}"
+alt="Здесь должна была быть картинка, но не нашлась">
+
+
+
+<p class='red'>И крепка, как смерть, любовь</p>
+<p><\p>
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
