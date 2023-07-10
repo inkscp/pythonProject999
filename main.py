3a3017ff59dc9a9dd98593718b48485b927b98f3
Inna <inna.pshenichnikova@gmail.com>
2023-07-10 11:50:00 +0300

    hello start

--- main.py
+++ main.py
@@ -22,10 +22,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/')  #декораторы
 @app.route('/index')
 def index():
     return 'hello'
+
+@app.route('/countdown')
+def countdown():
+    lst = [str(x) for x in range(10, 0, -1)]
+    lst.append('Start')
+    return '<br>'.join(lst)  #br - перенос на новую строку
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
