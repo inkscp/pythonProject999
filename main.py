eea2fe1013e9025e0eb8c1101d4966d9be76d902
Inna <inna.pshenichnikova@gmail.com>
2023-07-10 11:13:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# каркасный вариант приложения
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
