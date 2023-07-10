fe9a9c0de688d1603186a3f9debb722fccd9d54f
Inna <inna.pshenichnikova@gmail.com>
2023-07-10 14:59:25 +0300

    static - username - Error

--- main.py
+++ main.py
@@ -20,7 +20,7 @@
 # каркасный вариант приложения
 from flask import Flask, url_for  # подключаем конструктор и урл
 #
-# app = Flask(__name__)
+app = Flask(__name__)
 #
 # @app.route('/')  #декораторы
 # @app.route('/index')
@@ -62,8 +62,29 @@ from flask import Flask, url_for  # подключаем конструктор 
 #     return 'Ибо крепка, как смерть, любовь<br><a href="/">Назад</a>'
 
 
+
+@app.route('/greeting/<username>')  # строковый параметр
+def greeting(username):
+    return f"""<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <title>{username}</title>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}">
+ </head>
+ <body>
+ <h1>Привет, {username}</h1>
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+ </body>
+ </html>"""
+
+
+
 from flask import Flask, url_for
 
+app = Flask(__name__)
 
 @app.route('/nekrasov')
 def nekrasov():
